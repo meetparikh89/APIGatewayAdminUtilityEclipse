8c601f390f7ffa7a0ed0ba5ebd34a68dd9abfec0
Niharp111 <patil.nihar111@gmail.com>
2014-04-22 15:28:46 +0530

    Update CreateClientController.js

--- WebContent/scripts/controllers/CreateClientController.js
+++ WebContent/scripts/controllers/CreateClientController.js
@@ -19,61 +19,13 @@
 // });
 AdminUtilityApp
 		.controller(
-				'UpdateClientController',
-				function($scope, $http, $routeParams, $location,
-						ConfirmationDataFactory) {
-					var OldIPranges = [];
-					$scope.clientName = $routeParams.client;
-					$scope.IPranges =[{
-						'id' : "IP1"
-					}];
-					console.log("Update Client Controller is called");
-					console.log("Client NAme is "+$scope.clientName);
+				'CreateClientController',
+				function($scope, $http, $location, ConfirmationDataFactory) {
 					
-					if($scope.clientName != null && $scope.clientName!= undefined && $scope.clientName != ''){
-						
-						ConfirmationDataFactory.setUpdateRequest();
-						$scope.ClientTextBox=true;
-						$http({
-							method : 'GET',
-							url : 'admin/client/' + $scope.clientName + '/iprange'
-						}).success(function(data) {
-							console.log(JSON.stringify(data));
-
-							OldIPranges = angular.copy(data);
-							console.log("data value "+JSON.stringify(data));
-							if(data != null && data !=''){
-								console.log("Inside data");
-								$scope.IPranges = data;
-								
-							}
-							else{
-								$scope.IPranges =[{
-									'id' : "IP1"
-								}];
-							}
-						}).error(function(data) {
-							console.log('error');
-
-						});
-					}
-				
-					console.log("is Updated Request ="+ConfirmationDataFactory.getUpdateRequest());
-					if(ConfirmationDataFactory.getUpdateRequest()){
-					$scope.checkClient = function() {
-						console.log("Inside checkClient");
-						$http({
-							method : 'GET',
-							url : 'admin/client/' + $scope.clientName
-						}).success(function(data) {
-							alert("Client is Already Present");
-							$scope.clientName = "";
-						}).error(function(data) {
-							console.log('error');
-
-						});
-					};	
-					}
+					$scope.IPranges = [ {
+						'id' : "IP1"
+					} ];
+					
 					$scope.addNewIPRange = function() {
 						var newItemNo = $scope.IPranges[$scope.IPranges.length - 1].id + 1;
 						$scope.IPranges.push({
@@ -93,97 +45,40 @@ AdminUtilityApp
 						return !(IPrange.id === $scope.IPranges[$scope.IPranges.length - 1].id);
 					};
 
-					console.log($scope.clientName);
+					// $scope.checkClient = function() {
+					// 	$http({
+					// 		method : 'GET',
+					// 		url : 'admin/client/' + $scope.clientName
+					// 	}).success(function(data) {
+					// 		alert("Client is Already Present");
+					// 		$scope.name = "";
+					// 	}).error(function(data) {
+					// 		console.log('error');
+
+					// 	});
+					// };
 					$scope.createClient = function() {
-						if(ConfirmationDataFactory.getUpdateRequest()){
-						var myMap = new Map();
-						var deleteArray = [];
 						var addArray = [];
-						var ignoreArray = [];
-						for (var i = 0; i < $scope.IPranges.length; i++) {
-							myMap.set($scope.IPranges[i].name,
-									$scope.IPranges[i]);
-							addArray.push(myMap.get($scope.IPranges[i].name));
-							console.log("Set in the map"
-									+ $scope.IPranges[i].name);
-						}
-						var AllIpArray = angular.copy(addArray);
-						ConfirmationDataFactory.setAllIPRanges(AllIpArray);
-						for (var i = 0; i < OldIPranges.length; i++) {
-							console.log("Old Ip ranges inside for "
-									+ JSON.stringify(OldIPranges));
-							if (myMap.has(OldIPranges[i].name)) {
-								console.log("Found in the map"
-										+ OldIPranges[i].name);
-								var newIP = myMap.get(OldIPranges[i].name);
-								console.log(JSON.stringify(OldIPranges[i]));
-								console.log(JSON.stringify(newIP));
-								if (angular.equals(newIP.from,
-										OldIPranges[i].from)
-										&& angular.equals(newIP.to,
-												OldIPranges[i].to)) {
-									console.log("Inside IF the map"
-											+ OldIPranges[i].name);
-									ignoreArray.push(myMap.get(newIP.name));
-									addArray.splice(myMap.get(newIP.name), 1);
-
-								} else {
-
-									deleteArray.push(myMap
-											.get(OldIPranges[i].name));
-									addArray.push(myMap.get(newIP.name));
-								}
-							} else {
-								console.log("i ="+i+"deleted Element " + JSON.stringify(OldIPranges[i]));
-								deleteArray
-										.push(OldIPranges[i]);
-							}
-						}
-
-						console.log("Adding elements " + addArray);
-						console.log("Deleting elements " + deleteArray);
-						console.log("Existing elements " + ignoreArray);
-						var validity = document.getElementById("validityCombo");
-						var timeSpan = $scope.validityPeriod;
-						ConfirmationDataFactory.setAddValidityPeriod(timeSpan);
-
-						var timeUnit = validity.value;
-						ConfirmationDataFactory.setAddValidityUnit(timeUnit);
-						ConfirmationDataFactory
-								.setClientName($scope.clientName);
-						ConfirmationDataFactory.setPassword($scope.password);
-						ConfirmationDataFactory.setDeleteIPRanges(deleteArray);
-						ConfirmationDataFactory.setAddIPRanges(addArray);
-						ConfirmationDataFactory.setUpdateRequest();
-						$location.path("/confirmationPage");
-					};
-					
-					
-					if(ConfirmationDataFactory.getUpdateRequest()==false){
-						var addArray = [];
-						console.log("IPRangeCheckBox is "+$scope.IPRangeCheckBox);
+						console.log("IPRangeCheckBox" + $scope.IPRangeCheckBox);
 						if ($scope.IPRangeCheckBox) {
-							
+
 							for (var i = 0; i < $scope.IPranges.length; i++) {
 								addArray.push($scope.IPranges[i]);
 
 							}
 						}
-						console.log("Adding elements " + JSON.stringify(addArray));
+						console.log("Adding elements " + addArray);
 						var validity = document.getElementById("validityCombo");
 						var timeSpan = $scope.validityPeriod;
 						ConfirmationDataFactory.setAddValidityPeriod(timeSpan);
 
 						var timeUnit = validity.value;
 						ConfirmationDataFactory.setAddValidityUnit(timeUnit);
-						ConfirmationDataFactory
-								.setClientName($scope.clientName);
+						ConfirmationDataFactory.setClientName($scope.clientName.toLowerCase());
 						ConfirmationDataFactory.setPassword($scope.password);
 						ConfirmationDataFactory.setAddIPRanges(addArray);
 						ConfirmationDataFactory.setAllIPRanges(addArray);
+						ConfirmationDataFactory.setUpdateRequest(false);
 						$location.path("/confirmationPage");
-						
-					};
-					
 					};
 				});
